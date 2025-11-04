e9dd80376b205641c6fb38f456cb40df8b7c54b0
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-11-04 09:50:34 +0530

    start new

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const cors=require("cors")  //it is use for fetch data from database in frontend
+
+//app config
+const app=express();
+const port = process.env.PORT || 9000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+
+//MockMate
+//database connection
+const mongoose = require("mongoose");
+ const connectDB = async () => {
+    try {
+      await mongoose.connect("mongodb+srv://deepumelkani123_db_user:Bias77@cluster0.4gh7kmj.mongodb.net/?appName=Cluster0");
+      console.log("Database connected successfully");
+    } catch (error) {
+      console.error("Error connecting to database:", error);
+      process.exit(1);
+    }
+  };
+//db connectin model
+connectDB();
