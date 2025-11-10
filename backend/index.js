103bc9cabd3f426fd0f9f1924616f0737e39ef93
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-11-10 22:36:02 +0530

    set

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000", 
-    "https://biasonlinegrievancesystem.vercel.app"
+    "https://biasonlinegrievancesystem.vercel.app",
+    "http://89.233.104.66:4173"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
